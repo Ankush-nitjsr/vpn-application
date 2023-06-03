127cb1a135a3340b20038ba008ed7374d28186f2
Ankush <ankushnitjsr@gmail.com>
2023-06-03 21:40:32 +0530

    final commit

---
+++ src/main/java/com/driver/model/ServiceProvider.java
@@ -0,0 +1,83 @@
+package com.driver.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "serviceProvider")
+public class ServiceProvider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "Name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn
+    private Admin admin;
+
+    @OneToMany(mappedBy = "serviceProvider", cascade = CascadeType.ALL)
+    private List<Connection> connections = new ArrayList<>();
+
+    @OneToMany(mappedBy = "serviceProvider", cascade = CascadeType.ALL)
+    private List<Country> countries = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "serviceProvider", cascade = CascadeType.ALL)
+    private List<User> users = new ArrayList<>();
+
+    public ServiceProvider(String name) {
+        this.name = name;
+    }
+
+    public ServiceProvider() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    public List<Connection> getConnections() {
+        return connections;
+    }
+
+    public void setConnections(List<Connection> connections) {
+        this.connections = connections;
+    }
+
+    public List<Country> getCountries() {
+        return countries;
+    }
+
+    public void setCountries(List<Country> countries) {
+        this.countries = countries;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
